6c4f231a20f164fa9cac5999528bd2528da5b1e3
liam <liachenxiexu@gmail.com>
2021-05-19 09:02:50 +1000

    add normalize css

--- dist/static/js/main.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpextension"] = this["webpackJsonpextension"] || []).push([["main"],[],[[2,"runtime-main",0,2]]]);
-//# sourceMappingURL=main.chunk.js.map
--- src/options.js
+++ src/options.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './views/Options/App';
-// import * as serviceWorker from './serviceWorker';
+import 'normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
