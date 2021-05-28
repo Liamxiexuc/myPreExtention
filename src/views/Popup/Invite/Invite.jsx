4e8ec9d8e6b97b50505c348ff90115203bed8ffb
liam <liachenxiexu@gmail.com>
2021-05-28 15:26:33 +1000

    update invite page css

--- src/views/Popup/Invite/Invite.jsx
+++ src/views/Popup/Invite/Invite.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import formCreate from '../components/formCreate.js';
+import Layout from '../components/Layout.jsx';
+import Container from '../components/Container';
 import styles from './Invite.module.css';
 
 const nameRules = { required: true, message: 'please input ur name' };
@@ -27,33 +29,74 @@ class Invite extends Component {
   render() {
     const { getFieldDecorator } = this.props;
     return (
-      <div className={styles.invite}>
-        <h3>Invite Page - Invite other "power" users</h3>
-        {getFieldDecorator('name', { rules: [nameRules] })(
-          <input
-            className={styles.input}
-            type="email"
-            placeholder="please input ur first email"
-          />,
-        )}
-        {getFieldDecorator('password', { rules: [nameRules] })(
-          <input
-            className={styles.input}
-            type="email"
-            placeholder="please input ur last email"
-          />,
-        )}
-        {getFieldDecorator('email', { rules: [passwordRules] })(
-          <input
-            className={styles.input}
-            type="email"
-            placeholder="please input ur email"
-          />,
-        )}
-        <button className={styles.btn} onClick={this.submit}>
-          Send Invitations
-        </button>
-      </div>
+      <Layout>
+        <h1 className={styles.title}>INVITE NEW "POWER" USERS</h1>
+        <Container style={{ backgroundColor: '#ffe610' }}>
+          <div className={styles.invite}>
+            <p className={styles.text}>
+              SHARE THE LOVE, INVITE YOUR FRIENDS AND RECEIVE SPECIAL
+              "POWER" FEATURES.
+            </p>
+            <p className={styles.text}>
+              ENTER EMAIL ADDRESS'S BELOW TO SEND INVITATIONS TO
+              PROPERTY POWER.
+            </p>
+            <form className={styles.form}>
+              <div className={styles.sole}>
+                <div className={styles.row}>
+                  <label htmlFor="email" className={styles.label}>
+                    EMAIL 1
+                  </label>
+                  {getFieldDecorator('email', { rules: [nameRules] })(
+                    <input
+                      id="email"
+                      className={styles.input}
+                      type="email"
+                      placeholder="please input ur email"
+                    />,
+                  )}
+                </div>
+                <small className={styles.msg}>error</small>
+              </div>
+              <div className={styles.sole}>
+                <div className={styles.row}>
+                  <label htmlFor="email" className={styles.label}>
+                    EMAIL 2
+                  </label>
+                  {getFieldDecorator('email', { rules: [nameRules] })(
+                    <input
+                      id="email"
+                      className={styles.input}
+                      type="email"
+                      placeholder="please input ur email"
+                    />,
+                  )}
+                </div>
+                <small className={styles.msg}>error</small>
+              </div>
+              <div className={styles.sole}>
+                <div className={styles.row}>
+                  <label htmlFor="email" className={styles.label}>
+                    EMAIL 3
+                  </label>
+                  {getFieldDecorator('email', { rules: [nameRules] })(
+                    <input
+                      id="email"
+                      className={styles.input}
+                      type="email"
+                      placeholder="please input ur email"
+                    />,
+                  )}
+                </div>
+                <small className={styles.msg}>error</small>
+              </div>
+              <button className={styles.btn} onClick={this.submit}>
+                Send Invitations
+              </button>
+            </form>
+          </div>
+        </Container>
+      </Layout>
     );
   }
 }
