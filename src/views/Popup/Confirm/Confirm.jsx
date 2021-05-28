cf54cac0b91c99fb7a5960c0ad36f9c20fbd9486
liam <liachenxiexu@gmail.com>
2021-05-28 11:58:25 +1000

    update confirm page

--- src/views/Popup/Confirm/Confirm.jsx
+++ src/views/Popup/Confirm/Confirm.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Layout from '../components/Layout.jsx';
+import Container from '../components/Container';
 import { Link } from 'react-router-dom';
 import styles from './Confirm.module.css';
 
 function Confirm() {
   return (
-    <Layout>
-      <div className={styles.confirm}>
-        <h1>Waitlist confirm Page</h1>
-        <a href="https//www.google.com"> Join Group</a>
-        <Link to="/login">LOGIN</Link>
-      </div>
+    <Layout btn="login">
+      <Container title="YOUR NEXT POWER MOVE">
+        <h1 className={styles.title}>CONGRATULATIONS!</h1>
+        <p className={styles.text}>YOUR ARE IN THE QUEUE...</p>
+        <p className={styles.text}>TAKE ME TO THE FACEBOOK GROUP</p>
+        <p className={styles.text}>THERE MIGHT BE A FAST WAY</p>
+        <p className={styles.text}>
+          JOIN OUR FACEBOOK GROUP AND ASK ONE OF THE MEMBERS TO SEND
+          YOU A "POWER" INVITE.
+        </p>
+        <p className={styles.text}>SIMPLE.</p>
+        <Link className={styles.btn} to="/login">
+          TAKE ME TO THE FACEBOOK GROUP
+        </Link>
+      </Container>
     </Layout>
   );
 }
