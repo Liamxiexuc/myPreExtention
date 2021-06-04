d047768ef1df0864ca07b22056e9c1978008c177
liam <liachenxiexu@gmail.com>
2021-06-04 11:04:37 +1000

    update dynamics change icon based on url

--- src/background.js
+++ src/background.js
@@ -1,8 +1,30 @@
 /*global chrome*/
 /*eslint no-undef: "error"*/
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.runtime.openOptionsPage(() =>
-    console.log('options page opened'),
-  );
+const updateIcon = (tabId, url) => {
+  let matching = false;
+
+  if (url && url.includes('realestate.com.au/property')) {
+    matching = true;
+  }
+
+  if (matching) {
+    chrome.action.setIcon({ path: 'icon16.png', tabId: tabId });
+    return;
+  }
+  chrome.action.setIcon({ path: 'icon16_gray.png', tabId: tabId });
+};
+
+chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
+  const { url } = tab;
+  updateIcon(tabId, url);
+});
+
+chrome.tabs.onActivated.addListener(function (info) {
+  setTimeout(() => {
+    chrome.tabs.get(info.tabId, function (tab) {
+      const { url } = tab;
+      updateIcon(info.tabId, url);
+    });
+  }, 100);
 });
