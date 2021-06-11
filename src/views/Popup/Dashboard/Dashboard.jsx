e1ed066627cb56cb73e4ec5dbc8b2600a4ec1ea3
liam <liachenxiexu@gmail.com>
2021-06-11 13:39:37 +1000

    add wave loader

--- src/views/Popup/Dashboard/Dashboard.jsx
+++ src/views/Popup/Dashboard/Dashboard.jsx
@@ -1,20 +1,24 @@
 /* global chrome */
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout.jsx';
 import Container from '../components/Container';
+import WaveLoading from '../components/WaveLoading';
 import { fetchMemberData } from '../../../utils/member.js';
 import styles from './Dashboard.module.css';
 
 function Dashboard() {
   const [name, setName] = useState('');
   const [properties, setProperties] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
 
   const getMember = async () => {
+    setIsloading(true);
     const memberData = await fetchMemberData();
     const { userId, viewedProperty } = memberData;
     setName(userId.fullname);
     setProperties(viewedProperty);
+    setIsloading(false);
   };
   useEffect(() => {
     getMember();
@@ -29,36 +33,42 @@ function Dashboard() {
     <Layout logout={true}>
       <Container style={{ backgroundColor: 'white' }}>
         <div className={styles.dashboard}>
-          <h1 className={styles.title}>WELCOME {name}!</h1>
-          <p className={styles.msg}>
-            UNLOCK THE ADVANCED POWER FEATURES...
-          </p>
-          <Link className={styles.btn} to="/invite">
-            INVITE ANOTHER POWER USER
-          </Link>
-          <h5 className={styles.subtitle}>
-            YOUR PREVIOUS PROPERTIES:
-          </h5>
-          <ul className={styles.list}>
-            {properties.map((item) => {
-              const { _id, address, suburb, url } = item;
-              return (
-                <li
-                  key={_id}
-                  className={styles.li}
-                  onClick={() => handleClick(url)}
-                >
-                  {`${address}, ${suburb}`}
-                </li>
-              );
-            })}
-          </ul>
-          <h5 className={styles.subtitle}>HOW TO USE</h5>
-          <p className={styles.msg}>
-            ONCE LOGGED IN, SIMPLY NAVIGATE TO A REALESTATE.COM.AU
-            LISTING TO LOAD UP ITS POWER DATA... THIS WINDOW WILL
-            UPDATE AUTOMATICALLY.
-          </p>
+          {isLoading ? (
+            <WaveLoading />
+          ) : (
+            <Fragment>
+              <h1 className={styles.title}>WELCOME {name}!</h1>
+              <p className={styles.msg}>
+                UNLOCK THE ADVANCED POWER FEATURES...
+              </p>
+              <Link className={styles.btn} to="/invite">
+                INVITE ANOTHER POWER USER
+              </Link>
+              <h5 className={styles.subtitle}>
+                YOUR PREVIOUS PROPERTIES:
+              </h5>
+              <ul className={styles.list}>
+                {properties.map((item) => {
+                  const { _id, address, suburb, url } = item;
+                  return (
+                    <li
+                      key={_id}
+                      className={styles.li}
+                      onClick={() => handleClick(url)}
+                    >
+                      {`${address}, ${suburb}`}
+                    </li>
+                  );
+                })}
+              </ul>
+              <h5 className={styles.subtitle}>HOW TO USE</h5>
+              <p className={styles.msg}>
+                ONCE LOGGED IN, SIMPLY NAVIGATE TO A REALESTATE.COM.AU
+                LISTING TO LOAD UP ITS POWER DATA... THIS WINDOW WILL
+                UPDATE AUTOMATICALLY.
+              </p>
+            </Fragment>
+          )}
         </div>
       </Container>
     </Layout>
---
+++ src/views/Popup/components/WaveLoading/WaveLoading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './WaveLoading.module.css';
+
+function WaveLoading() {
+  return <div className={styles.loader}></div>;
+}
+
+export default WaveLoading;
