0a1d267a714a05472cf55b91ea335eb1b68bb216
liam <liachenxiexu@gmail.com>
2021-07-01 11:10:54 +1000

    update page loading

--- src/views/Popup/Property/Authed.jsx
+++ src/views/Popup/Property/Authed.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Redirect } from 'react-router-dom';
 import Layout from '../components/Layout.jsx';
 import Card from './components/Card.jsx';
@@ -38,27 +38,30 @@ const Authed = () => {
   return (
     <Layout lightning={true} logout={true} page={'authed'}>
       {isError && <Redirect to="/error" />}
-      <h1
-        className={styles.title}
-      >{`${data.address}, ${data.suburb}`}</h1>
-      <main className={styles.property}>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <section className={styles.container}>
-            {propertyData &&
-              propertyData.map((i) => (
-                <Card
-                  key={i.title}
-                  title={i.title}
-                  data={i.data}
-                  active={active}
-                  setActive={setActive}
-                />
-              ))}
-          </section>
-        )}
-      </main>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Fragment>
+          <h1
+            className={styles.title}
+          >{`${data.address}, ${data.suburb}`}</h1>
+          <main className={styles.property}>
+            <section className={styles.container}>
+              {propertyData &&
+                propertyData.map((i) => (
+                  <Card
+                    key={i.title}
+                    title={i.title}
+                    data={i.data}
+                    active={active}
+                    setActive={setActive}
+                  />
+                ))}
+            </section>
+          </main>
+        </Fragment>
+      )}
     </Layout>
   );
 };
