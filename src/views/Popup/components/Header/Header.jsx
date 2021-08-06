86486c86c248b4d42359d8d01c39b4905218d89c
liam <liachenxiexu@gmail.com>
2021-08-06 14:57:57 +1000

    update style

--- src/views/Popup/components/Header/Header.jsx
+++ src/views/Popup/components/Header/Header.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
-import bannerText from '../../../../assets/banner_text.png';
+import banner from '../../../../assets/banner.png';
 
 const Header = () => (
   <header className={styles.header}>
     <img
       className={styles.logo}
-      src={bannerText}
-      alt="banner_text"
+      src={banner}
+      alt="property power banner"
     ></img>
-    <div className={styles.betaContainer}>
-      <span className={styles.text}>BETA</span>
-    </div>
   </header>
 );
 
